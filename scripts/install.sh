cb140950ed26df5a7ab81d1714567902a73588c3
Gheorghe Stancov <gs@smartsoftdev.eu>
2022-05-04 09:11:02 +0200

    swithc to readlink -f

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR=$(readlink -e $(dirname ${BASH_SOURCE[0]})/../)
+DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]})/../)
 
 sudo ln -sf $DIR/src/git_component.py /bin/git_component
 sudo ln -sf $DIR/src/gen_changelog_simple_html.py /bin/gen_changelog_simple_html
